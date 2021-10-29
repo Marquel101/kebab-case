8ed2eb774c5aed2645fe89df05ab2dc0c66c0cf2
Marquel101 <daylensears2@gmail.com>
2021-10-29 10:35:03 -0400

    Anotha 1

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ import { VscSmiley } from "react-icons/vsc"
 const Home = () => {
     return (
         <div className="intro">
-            <h6 id="intro">
+            <h6>
             Tough Talk is a fun an interactive website where you can see all the different views people have on certain topics. We offer a wide variety of discussion topics for you to choose from. We recommend to do this with a group of friends, but if you don't have any no worries<VscSmiley />.</h6>
         </div>
     )
