a93d64d91049adfa9c7a1fb1fbed176ad64d394d
Marquel101 <daylensears2@gmail.com>
2021-10-29 09:41:41 -0400

    Anotha 1

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,10 @@
+import { VscSmiley } from "react-icons/vsc"
+
 const Home = () => {
     return (
         <div className="intro">
             <h6 id="intro">
-            Tough Talk is a fun an interactive website where you can see all the different views people have on certain topics. We offer a wide variety of discussion topics for you to choose from. We even offer you the ability to input your own opinions! Using an Airtable API you get to submit your own api based on the available categories we have.
-            </h6>
+            Tough Talk is a fun an interactive website where you can see all the different views people have on certain topics. We offer a wide variety of discussion topics for you to choose from. We recommend to do this with a group of friends, but if you don't have any no worries<VscSmiley />.</h6>
         </div>
     )
 }
